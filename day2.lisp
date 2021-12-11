4436d38b0c5f1ba6c1422cd59f4c3a140f3845d9
Matthew Curry <mjcurry@gmail.com>
2021-12-10 20:41:42 -0800

    finish day2

--- day2.lisp
+++ day2.lisp
@@ -1,33 +1,74 @@
 (in-package #:day2)
 
 (coalton-toplevel
-  (define-type Direction Forward Up Down)
-  (define-type Command (Command (Tuple Direction Integer)))
+  (define-type Command
+    (Forward Integer)
+    (Up Integer)
+    (Down Integer))
 
-  (declare parse-direction (String -> (Optional Direction)))
-  (define (parse-direction str)
-    (match str
-      ("forward" (Some Forward))
-      ("up" (Some Up))
-      ("down" (Some Down))
-      (_ None)))
-
-  (declare parse-command (String -> (Optional Command)))
-  (define (parse-command line)
-    (match (split #\Space line)
-      ((Cons dir (Cons num (Nil)))
-       (match (Tuple (parse-direction dir) (parse-int num))
-         ((Tuple (Some d) (Some n))
-          (Some (Command (Tuple d n))))
+  (declare parse-direction (String -> (Optional Integer) -> (Optional Command)))
+  (define (parse-direction str num)
+    (match num
+      ((Some n)
+       (match str
+         ("forward" (Some (Forward n)))
+         ("up" (Some (Up n)))
+         ("down" (Some (Down n)))
          (_ None)))
       (_ None)))
+
+  (declare read-command (String -> (Optional Command)))
+  (define (read-command line)
+    (match (split #\Space line)
+      ((Cons dir (Cons num (Nil)))
+       (parse-direction dir (parse-int num)))
+      (_ None)))
+
+  (declare read-commands (String -> (List Command)))
+  (define (read-commands pathname)
+    (let ((only-some (fn (c)
+                       (match c
+                         ((Some x) x))))
+          (lines (lisp (List String) (pathname)
+                    (adventofcode2021:read-lines pathname))))
+      (map only-some
+           (filter issome
+                   (map read-command lines)))))
+
+  (declare horiz-x-depth ((List Command) -> Integer))
+  (define (horiz-x-depth commands)
+    (let ((inner (fn (h d cmds)
+                   (match cmds
+                     ((Nil) (* h d))
+                     ((Cons c cs)
+                      (match c
+                        ((Forward x) (inner (+ h x) d cs))
+                        ((Up x)      (inner h (- d x) cs))
+                        ((Down x)    (inner h (+ d x) cs))))))))
+      (inner 0 0 commands)))
+
+  (declare depth-score ((List Command) -> Integer))
+  (define (depth-score commands)
+    (let ((inner (fn (a h d cmds)
+                   (match cmds
+                     ((Nil) (* h d))
+                     ((Cons c cs)
+                      (match c
+                        ((Forward x) (inner a       (+ h x) (+ d (* a x)) cs))
+                        ((Up x)      (inner (- a x) h       d cs))
+                        ((Down x)    (inner (+ a x) h       d cs))))))))
+      (inner 0 0 0 commands)))
 
   #+nil nil)
 
 #+nil
 (
- (coalton (parse-command "forward 5"))
- (coalton (map (+ 2) (Cons 1 (Cons 2 (Cons 3 Nil)))))
+ (coalton (read-command "forward 5"))
 
- (coalton (filter issome (Cons (parse-command "forward 4") Nil)))
+ (coalton (map (fn (c) (match c ((Some x) x))) (filter issome (Cons (parse-direction "forward 4") Nil))))
+
+ (coalton (horiz-x-depth (read-commands "day2input")))
+ (coalton (horiz-x-depth (make-list (Forward 5) (Down 4))))
+
+ (coalton (depth-score (read-commands "day2input")))
  nil)
